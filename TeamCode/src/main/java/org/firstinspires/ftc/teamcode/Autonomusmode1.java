031356c7921c94b7acfb511cb96fd32b19a756c3
maxpavlo0@gmail.com <maxpavlo0@gmail.com>
2017-02-05 21:39:56 +0200

    gyhrtu

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomusmode1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomusmode1.java
@@ -14,17 +14,20 @@ public class Autonomusmode1 extends LinearOpMode {
         RobotClass robot = new RobotClass(hardwareMap);
 
         waitForStart();
-        robot.moveForwardEncoders(4320, 0.7, telemetry);
-        robot.turnLeftEncoders(1000,0.5);
+        
+
+
+        robot.moveForwardEncoders(4260,0.7,telemetry);
+        robot.turnLeft(300,0.4);
+        robot.moveForwardEncoders(400,0.6,telemetry);
+        robot.moveBackwardsEncoders(500,0.6);
+        robot.turnLeft(200,0.4);
         robot.moveForwardEncoders(300,0.8,telemetry);
-        robot.moveBackwardsEncoders(300,0.8);
-        robot.turnLeftEncoders(1000,0.8);
-        robot.moveBackwardsEncoders(3000,0.7);
-        robot.turnLeftEncoders(1000,0.4);
-        robot.moveForwardEncoders(1000,0.4,telemetry);
-        robot.turnLeftEncoders(1000,0.2);
-        robot.moveBackwardsEncoders(500,1);
-        robot.suck(-1);
+        robot.turnLeft(300,0.5);
+        robot.moveForwardEncoders(400,0.5,telemetry);
+        robot.suck(1);
+
+
         /** Robot Start Position
              //.     .
          //(.)
